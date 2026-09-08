4d330789fca960160e9ed3df58ca967eba9cb654
LaiVanChung <126126468+tothichcau02@users.noreply.github.com>
2026-09-08 11:17:41 +0700

    fix carousel

--- src/data/heroSlides.ts
+++ src/data/heroSlides.ts
@@ -8,19 +8,19 @@ export interface HeroSlide {
 export const heroSlides: HeroSlide[] = [
   {
     id: 1,
-    image: "/images/banners/banner1.png",
+    image: "/images/banners/banner1.jpg",
     alt: "Sản phẩm và nhà máy Công ty Thái Sơn",
     href: "#bang-dinh",
   },
   {
     id: 2,
-    image: "/images/banners/banner2.png",
+    image: "/images/banners/banner2.jpg",
     alt: "Các sản phẩm vật tư đóng gói",
     href: "#mang-pe",
   },
   {
     id: 3,
-    image: "/images/banners/banner3.png",
+    image: "/images/banners/banner3.jpg",
     alt: "Giải pháp đóng gói công nghiệp",
     href: "#lien-he",
   },
